69d991cf065ac8373bfbaf8a14eb4f3259bda859
Alan Beveridge <adgbeveridge@proton.me>
2023-09-22 18:37:09 -0700

    Add dynamically created components

---
+++ src/component.rs
@@ -0,0 +1,94 @@
+// Represents the component images (like a capacitor)
+#[derive(Default)]
+struct Download {
+    id: usize,
+    state: State,
+}
+
+
+impl Download {
+    pub fn new(id: usize) -> Self {
+        Download::default()
+    }
+
+    pub fn start(&mut self) {
+        match self.state {
+            State::Idle { .. }
+            | State::Finished { .. }
+            | State::Errored { .. } => {
+                self.state = State::Downloading { progress: 0.0 };
+            }
+            State::Downloading { .. } => {}
+        }
+    }
+
+    pub fn progress(&mut self, new_progress: download::Progress) {
+        if let State::Downloading { progress } = &mut self.state {
+            match new_progress {
+                download::Progress::Started => {
+                    *progress = 0.0;
+                }
+                download::Progress::Advanced(percentage) => {
+                    *progress = percentage;
+                }
+                download::Progress::Finished => {
+                    self.state = State::Finished;
+                }
+                download::Progress::Errored => {
+                    self.state = State::Errored;
+                }
+            }
+        }
+    }
+
+    pub fn subscription(&self) -> Subscription<Message> {
+        match self.state {
+            State::Downloading { .. } => {
+                download::file(self.id, "https://speed.hetzner.de/100MB.bin?")
+                    .map(Message::DownloadProgressed)
+            }
+            _ => Subscription::none(),
+        }
+    }
+
+    pub fn view(&self) -> Element<Message> {
+        let current_progress = match &self.state {
+            State::Idle { .. } => 0.0,
+            State::Downloading { progress } => *progress,
+            State::Finished { .. } => 100.0,
+            State::Errored { .. } => 0.0,
+        };
+
+        let progress_bar = progress_bar(0.0..=100.0, current_progress);
+
+        let control: Element<_> = match &self.state {
+            State::Idle => button("Start the download!")
+                .on_press(Message::Download(self.id))
+                .into(),
+            State::Finished => {
+                column!["Download finished!", button("Start again")]
+                    .spacing(10)
+                    .align_items(Alignment::Center)
+                    .into()
+            }
+            State::Downloading { .. } => {
+                text(format!("Downloading... {current_progress:.2}%")).into()
+            }
+            State::Errored => column![
+                "Something went wrong :(",
+                button("Try again").on_press(Message::Download(self.id)),
+            ]
+            .spacing(10)
+            .align_items(Alignment::Center)
+            .into(),
+        };
+
+        Column::new()
+            .spacing(10)
+            .padding(10)
+            .align_items(Alignment::Center)
+            .push(progress_bar)
+            .push(control)
+            .into()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
-use iced::widget::{text, text_input, Column, Container, Row, Text, TextInput};
+use iced::widget::{Button, Column, Container, Text, TextInput};
 use iced::{Element, Sandbox, Settings};
 
 pub fn main() -> iced::Result {
-    Progress::run(Settings::default())
-}
-
-#[derive(Default)]
-struct Progress {
-    input: String,
+    Creator::run(Settings::default())
 }
 
 #[derive(Debug, Clone)]
 enum Message {
     TextInputChanged(String),
+    SaveClicked,
+    AddComponent,
 }
 
-impl Sandbox for Progress {
+#[derive(Default)]
+struct Creator {
+    input: String,
+    //components: Vec<>,
+}
+
+
+impl Sandbox for Creator {
     type Message = Message;
 
     fn new() -> Self {
@@ -29,52 +33,100 @@ impl Sandbox for Progress {
     fn update(&mut self, message: Message) {
         match message {
             Message::TextInputChanged(x) => self.input = x,
+            Message::SaveClicked => println!("Clicked"),
+            Message::AddComponent => add_component(self),
         }
     }
 
     fn view(&self) -> Element<Message> {
+        // View title
         let title: Text = Text::new("Enter Configuration File Values")
             .width(iced::Length::Fill)
             .size(25)
             .horizontal_alignment(iced::alignment::Horizontal::Center);
 
-        // Component filename row.
-        let component_filename_help: Text = Text::new("Path to component image")
-            .width(iced::Length::Fill)
-            .height(iced::Length::Fill)
-            .horizontal_alignment(iced::alignment::Horizontal::Right)
+        // PCB Name column.
+        let pcb_name_help: Text = Text::new("Name of the PCB")
+            .horizontal_alignment(iced::alignment::Horizontal::Left)
             .vertical_alignment(iced::alignment::Vertical::Top);
 
-        let component_filename_input: TextInput<Message> =
-            TextInput::new("Set filename to component image", &self.input)
+        let pcb_name_input: TextInput<Message> =
+            TextInput::new("Enter the name of the printed circuit board", &self.input)
                 .on_input(Message::TextInputChanged);
 
-        // Component filename row.
-        let threshold_help: Text = Text::new("Path to component image")
+        // Save button
+        let save_button: Button<Message> = Button::new("Save")
+            .on_press(Message::SaveClicked)
+            .padding([10, 40, 10, 40]);
+        let button_container = Container::new(save_button)
+            .height(iced::Length::Fill)
+            .align_y(iced::alignment::Vertical::Bottom)
+            .center_x()
+            .width(iced::Length::Fill);
+
+        // Add new component column
+        let add_button: Button<Message> = Button::new("Add New Component")
+            .on_press(Message::SaveClicked)
+            .padding([5, 10, 5, 10]);
+
+        let add_container = Container::new(add_button)
+            .center_x()
             .width(iced::Length::Fill)
-            .horizontal_alignment(iced::alignment::Horizontal::Right)
-            .vertical_alignment(iced::alignment::Vertical::Center);
+            .padding(10);
 
-        let threshold_input: TextInput<Message> =
-            TextInput::new("Set filename to component image", &self.input)
-                .on_input(Message::TextInputChanged);
-
-        let component_row: Row<Message> = Row::new()
-            .push(component_filename_help)
-            .push(component_filename_input);
-
-        let threshold_row: Row<Message> = Row::new().push(threshold_help).push(threshold_input);
+        // Bring the info text and the placeholder text into a column
+        let pcb_name_column: Column<Message> = Column::new()
+            .push(pcb_name_help)
+            .push(pcb_name_input)
+            .padding(5);
 
         let window_box = Container::new(
             Column::new()
+                .height(iced::Length::Fill)
                 .width(iced::Length::Fill)
                 .push(title)
-                .push(component_row)
-                .push(threshold_row),
+                .push(pcb_name_column)
+                .push(add_container)
+                .push(button_container),
         )
         .padding(5)
         .into();
 
         window_box
     }
+}
+
+fn add_component(app: &Creator){
+    // Component filename column.
+    let component_filename_help: Text = Text::new("Filename for the master component image")
+    .horizontal_alignment(iced::alignment::Horizontal::Left)
+    .vertical_alignment(iced::alignment::Vertical::Top);
+
+    let component_filename_input: TextInput<Message> =
+    TextInput::new("Enter filename for master component image", &app.input)
+        .on_input(Message::TextInputChanged);
+
+    // Component threshold row.
+    let threshold_help: Text =
+    Text::new("Matching threshold for master component image vs. test board component")
+        .width(iced::Length::Fill)
+        .horizontal_alignment(iced::alignment::Horizontal::Left)
+        .vertical_alignment(iced::alignment::Vertical::Center);
+
+    let threshold_input: TextInput<Message> =
+    TextInput::new("Set component comparison threshold number", &app.input)
+        .on_input(Message::TextInputChanged);
+
+
+    let component_column: Column<Message> = Column::new()
+        .push(component_filename_help)
+        .push(component_filename_input)
+        .padding(5);
+
+    let threshold_column: Column<Message> = Column::new()
+        .push(threshold_help)
+        .push(threshold_input)
+        .padding(5);
+
+    app.window_box
 }
