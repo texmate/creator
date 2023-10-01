ec468d6632c03f3faf0bf37f72b4203230f8298c
Alan Beveridge <adgbeveridge@proton.me>
2023-10-01 13:38:31 -0700

    Implement dynamic component adding

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,25 @@ enum Message {
     TextInputChanged(String),
     SaveClicked,
     AddComponent,
+    ComponentInputChanged { value: String, id: usize },
 }
 
-#[derive(Default)]
 struct Creator {
     input: String,
-    //components: Vec<>,
+    //components: Vec<component::Component>,
+    component_num: i32, // Keeps how many components are on this board. 
+    component_inputs: Vec<String>, // All the values of the inputs. 
 }
-
 
 impl Sandbox for Creator {
     type Message = Message;
 
     fn new() -> Self {
-        Self::default()
+        Self {
+            component_num: 0,
+            input: String::new(),
+            component_inputs: Vec::new(),
+        }
     }
 
     fn title(&self) -> String {
@@ -35,15 +40,24 @@ impl Sandbox for Creator {
             Message::TextInputChanged(x) => self.input = x,
             Message::SaveClicked => println!("Clicked"),
             Message::AddComponent => add_component(self),
+            Message::ComponentInputChanged { value, id } => {
+                self.component_inputs[id] = value;
+            }
         }
     }
 
     fn view(&self) -> Element<Message> {
+        let mut content: Column<Message> = Column::new()
+            .height(iced::Length::Fill)
+            .width(iced::Length::Fill)
+            .padding(5);
+
         // View title
         let title: Text = Text::new("Enter Configuration File Values")
             .width(iced::Length::Fill)
             .size(25)
             .horizontal_alignment(iced::alignment::Horizontal::Center);
+        content = content.push(title);
 
         // PCB Name column.
         let pcb_name_help: Text = Text::new("Name of the PCB")
@@ -54,6 +68,44 @@ impl Sandbox for Creator {
             TextInput::new("Enter the name of the printed circuit board", &self.input)
                 .on_input(Message::TextInputChanged);
 
+        let pcb_name_column: Column<Message> = Column::new()
+            .push(pcb_name_help)
+            .push(pcb_name_input)
+            .padding(5);
+        content = content.push(pcb_name_column);
+
+        // The dynamic list of components.
+
+        let mut components: Column<Message> = Column::new();
+        for current_num in 0..self.component_num {
+            let id = current_num as usize;
+
+            println!("{}", id);
+            // Create a text input. Use a closure to pass the id of the the text input when stuff is typed in.
+            let res: TextInput<'_, Message> = TextInput::new(
+                "Enter the name of the printed circuit board",
+                &self.component_inputs[id],
+            ) // Now that its created, deal with the text updates. 
+            .on_input(move |new_value| Message::ComponentInputChanged {
+                value: new_value.clone(),
+                id: id,
+            });
+            components = components.push(res);
+        }
+
+        content = content.push(components);
+
+        // The add new component button column
+        let add_button: Button<Message> = Button::new("Add New Component")
+            .on_press(Message::AddComponent)
+            .padding([5, 10, 5, 10]);
+
+        let add_container = Container::new(add_button)
+            .center_x()
+            .width(iced::Length::Fill)
+            .padding(10);
+        content = content.push(add_container);
+
         // Save button
         let save_button: Button<Message> = Button::new("Save")
             .on_press(Message::SaveClicked)
@@ -63,70 +115,13 @@ impl Sandbox for Creator {
             .align_y(iced::alignment::Vertical::Bottom)
             .center_x()
             .width(iced::Length::Fill);
+        content = content.push(button_container);
 
-        // Add new component column
-        let add_button: Button<Message> = Button::new("Add New Component")
-            .on_press(Message::SaveClicked)
-            .padding([5, 10, 5, 10]);
-
-        let add_container = Container::new(add_button)
-            .center_x()
-            .width(iced::Length::Fill)
-            .padding(10);
-
-        // Bring the info text and the placeholder text into a column
-        let pcb_name_column: Column<Message> = Column::new()
-            .push(pcb_name_help)
-            .push(pcb_name_input)
-            .padding(5);
-
-        let window_box = Container::new(
-            Column::new()
-                .height(iced::Length::Fill)
-                .width(iced::Length::Fill)
-                .push(title)
-                .push(pcb_name_column)
-                .push(add_container)
-                .push(button_container),
-        )
-        .padding(5)
-        .into();
-
-        window_box
+        content.into()
     }
 }
 
-fn add_component(app: &Creator){
-    // Component filename column.
-    let component_filename_help: Text = Text::new("Filename for the master component image")
-    .horizontal_alignment(iced::alignment::Horizontal::Left)
-    .vertical_alignment(iced::alignment::Vertical::Top);
-
-    let component_filename_input: TextInput<Message> =
-    TextInput::new("Enter filename for master component image", &app.input)
-        .on_input(Message::TextInputChanged);
-
-    // Component threshold row.
-    let threshold_help: Text =
-    Text::new("Matching threshold for master component image vs. test board component")
-        .width(iced::Length::Fill)
-        .horizontal_alignment(iced::alignment::Horizontal::Left)
-        .vertical_alignment(iced::alignment::Vertical::Center);
-
-    let threshold_input: TextInput<Message> =
-    TextInput::new("Set component comparison threshold number", &app.input)
-        .on_input(Message::TextInputChanged);
-
-
-    let component_column: Column<Message> = Column::new()
-        .push(component_filename_help)
-        .push(component_filename_input)
-        .padding(5);
-
-    let threshold_column: Column<Message> = Column::new()
-        .push(threshold_help)
-        .push(threshold_input)
-        .padding(5);
-
-    app.window_box
+fn add_component(app: &mut Creator) {
+    app.component_num += 1;
+    app.component_inputs.push(String::from(""));
 }
